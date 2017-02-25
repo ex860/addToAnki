3586f9ba2759f3fdabd220c3261e0e8eeb7ddf2e
hwchiu <hwchiu@cs.nctu.edu.tw>
2017-02-25 17:27:10 +0800

    Use the python script to add words
    Signed-off-by: hwchiu <hwchiu@cs.nctu.edu.tw>

--- examples/YahooDict/README.md
+++ examples/YahooDict/README.md
@@ -2,6 +2,12 @@ Introdution
 -----------
 This is a example of lookuping the word from yahoo dict (english->chinese traditional) and add it to your ankicollection.
 
+
+Requirement
+-----------
+- python3
+- BeautifulSoup
+- pip3 install lxml
 Content
 -------
 - Front Card:
--- examples/YahooDict/main.py
+++ examples/YahooDict/main.py
@@ -1,27 +1,34 @@
-import __builtin__
-import urllib2
-import os.path, time
-from dateutil import parser
+import urllib.request;
 from bs4 import BeautifulSoup
+import subprocess
 
-def LookUpFromYahoo(word):
+Collection="/Users/hwchiu/Library/Application Support/Anki2/User 1/collection.anki2"
+Deck="Novel"
+Anki="../../add_to_anki-2.0.12.py"
+
+def look_up_from_yahoo(word):
+    """This function will lookup the @input(word) from the yahoo dict"""
     url="https://tw.dictionary.yahoo.com/dictionary?p={}".format(word)
-    content = urllib2.urlopen(url).read()
+    content = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(content, 'lxml')
-    print 'current word = {}'.format(word)
+    front_word = word + "<br>"
+    back_word = ""
+
+    print('current word = {}'.format(word))
     for i in soup.find_all('span', {'id':'pronunciation_pos'}):
-        print i.get_text()
+        front_word += i.get_text()
     for i in soup.find_all('div', {'class':'explain'}):
         wordType = i.find_all('div', {'class':'compTitle'})
         wordDefi = i.find_all('ul', {'class':'compArticleList'})
         for i in range(0,len(wordType)):
-            print wordType[i].get_text()
+            back_word += wordType[i].get_text() +"<br>"
             for q in wordDefi[i]:
-                print q.get_text()
-    print '--------------------'
-
-with open("input", "r") as file:
+                back_word += q.get_text() + "<br>"
+    print('front card={}'.format(front_word))
+    print('back_card={}'.format(back_word))
+    if 0 == len(back_word):
+        return
+    subprocess.run(['python3', Anki, Collection, Deck, front_word, back_word])
+with open("./input", "r") as file:
     for word in file:
-        LookUpFromYahoo(word)
-
-
+        look_up_from_yahoo(word)
